29cdef92b033ae8dd2f1a8d99b61b9a3e297ac0d
bacdv8888 <doanvanbac8888@gmail.com>
2020-06-23 10:49:57 +0700

    123

--- ClassManagement/src/main/java/cmw/main/Management.java
+++ ClassManagement/src/main/java/cmw/main/Management.java
@@ -12,7 +12,7 @@ import cmw.utils.HibernateUtils;
 public class Management {
 
   public static void main(String[] args) {
-    // Position pos = new Position("Supervisor"); // testing123
+    // Position pos = new Position("Supervisor"); // testing1234
     PositionDAO pd = new PositionDAOImpl();
     SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
     Session session = sessionFactory.openSession();
