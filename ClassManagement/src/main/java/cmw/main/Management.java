f29a47845341a5f6a0f74f60e2c62e0c370100b4
Vincent.Ng <63837324+tuananh8497@users.noreply.github.com>
2020-06-23 14:00:23 +0700

    update main, person and position

--- ClassManagement/src/main/java/cmw/main/Management.java
+++ ClassManagement/src/main/java/cmw/main/Management.java
@@ -1,12 +1,17 @@
 package cmw.main;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import cmw.dao.PersonDAO;
 import cmw.dao.PersonDAOImpl;
 import cmw.dao.PositionDAO;
 import cmw.dao.PositionDAOImpl;
+import cmw.dao.SubjectDAO;
+import cmw.dao.SubjectDAOImpl;
+import cmw.models.Person;
 import cmw.models.Position;
+import cmw.models.Subject;
 import cmw.utils.HibernateUtils;
 
 public class Management {
@@ -14,11 +19,27 @@ public class Management {
   public static void main(String[] args) {
     // Position pos = new Position("Supervisor"); // added Manager into DB
     PositionDAO pd = new PositionDAOImpl();
+    SubjectDAO sd = new SubjectDAOImpl();
+    PersonDAO personDao = new PersonDAOImpl();
     SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    // session.save(pos);
-    System.out.println(pd.getPosition(1));
+    
+    List <Position> listPos = pd.getAllPosition();
+    for(Position a: listPos) {
+      System.out.println(a.toString());
+    }
+    
+//    List <Person> listPerson = personDao.getAllPerson();
+//    for(Person a: listPerson) {
+//      System.out.println(a);
+//    }
+    
+//    List <Subject> listSubject = sd.getAllSubject();
+//    for(Subject a: listSubject) {
+//      System.out.println(a);
+//    }
+    
     session.getTransaction().commit();
     HibernateUtils.shutdown();
 
